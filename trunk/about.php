6ed820c83fcce75dfdaf9c6455fdd9175f180fb7
Shecspi <Shecspi@cebd97f6-3ea8-11de-8fef-1f5ae417a2df>
2009-06-18 19:36:27 +0000

    * Реализован двупанельный интерфейс. * Некоторые функции вынесены в отдельные файлы

---
+++ trunk/about.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @copyright Copyright (C) 2009, Вавилов Егор (Shecspi)
+ * @license http://www.opensource.org/licenses/mit-license.php MIT License
+ * @link http://code.google.com/p/flight-files/ Домашняя страница проекта
+ */
+
+/**
+ * Функция выводит диалоговое окно, в котором указана
+ * информация о программе, разработчике и лицензии.
+ */
+function about()
+{
+    global $lang;
+    
+    $dialog = new GtkAboutDialog();
+    $dialog->set_skip_taskbar_hint(TRUE);
+    $dialog->set_icon(GdkPixbuf::new_from_file(ICON_PROGRAM));
+    $dialog->set_logo(GdkPixbuf::new_from_file(ICON_PROGRAM));
+    $dialog->set_program_name('FlightFiles');
+    $dialog->set_version(VERSION_PROGRAM);
+    $dialog->set_comments($lang['about']['comments']);
+    $dialog->set_copyright('Copyright © 2009 Shecspi');
+    $dialog->set_website('http://code.google.com/p/flight-files/');
+    $dialog->set_authors(array('Вавилов Егор (Shecspi) <shecspi@gmail.com>'));
+    $dialog->set_license($lang['about']['license']);
+    $dialog->run();
+    $dialog->destroy();
+}
