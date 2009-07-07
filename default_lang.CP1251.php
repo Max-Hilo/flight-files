2ea73a19480e2911142248e2da164e0610dd8c77
Shecspi <Shecspi@cebd97f6-3ea8-11de-8fef-1f5ae417a2df>
2009-07-07 13:43:02 +0000

    * Для Windows используется кодировка CP1251, а для остальных - UTF8.

---
+++ default_lang.CP1251.php
@@ -0,0 +1,254 @@
+<?php
+
+/**
+ * Файл локализации, используемый по умолчанию.
+ *
+ * @copyright Copyright (C) 2009, Вавилов Егор (Shecspi)
+ * @license http://www.opensource.org/licenses/mit-license.php MIT License
+ * @link http://code.google.com/p/flight-files/ Домашняя страница проекта
+ */
+
+$lang['title_program']                  = 'Фаловый менеджер FlightFiles';
+
+$lang['menu']['file']                   = '_Файл';
+$lang['menu']['edit']                   = '_Правка';
+$lang['menu']['view']                   = '_Вид';
+$lang['menu']['go']                     = 'Пере_ход';
+$lang['menu']['bookmarks']              = '_Закладки';
+$lang['menu']['help']                   = '_Справка';
+$lang['menu']['new_file']               = 'Создать файл';
+$lang['menu']['new_dir']                = 'Создать папку';
+$lang['menu']['clear_bufer']            = 'Очистить буфер обмена';
+$lang['menu']['comparison_file']        = 'Сравнить файлы';
+$lang['menu']['comparison_dir']         = 'Сравнить папки';
+$lang['menu']['active_all']             = 'Выделить все';
+$lang['menu']['active_all_none']        = 'Снять выделение';
+$lang['menu']['close']                  = 'Закрыть';
+$lang['menu']['copy']                   = 'Копировать';
+$lang['menu']['cut']                    = 'Вырезать';
+$lang['menu']['paste']                  = 'Вставить';
+$lang['menu']['rename']                 = 'Переименовать';
+$lang['menu']['mass_rename']            = 'Массовое переименование';
+$lang['menu']['preference']             = 'Параметры';
+$lang['menu']['toolbar_view']           = 'Панель инструментов';
+$lang['menu']['partbar_view']           = 'Список разделов';
+$lang['menu']['addressbar_view']        = 'Адресная строка';
+$lang['menu']['statusbar_view']         = 'Строка состояния';
+$lang['menu']['hidden_files']           = 'Показывать скрытые файлы';
+$lang['menu']['up']                     = 'Вверх';
+$lang['menu']['back']                   = 'Назад';
+$lang['menu']['forward']                = 'Вперёд';
+$lang['menu']['refresh']                = 'Обновить';
+$lang['menu']['not_bookmarks']          = 'Закладок нет';
+$lang['menu']['bookmarks_add']          = 'Добавить в закладки';
+$lang['menu']['bookmarks_edit']         = 'Изменить закладки';
+$lang['menu']['shortcuts']              = 'Сочетания клавиш';
+$lang['menu']['about']                  = 'О программе';
+
+$lang['toolbar']['back']                = 'Назад';
+$lang['toolbar']['back_hint']           = 'Перейти в предыдущую посещённую папку';
+$lang['toolbar']['forward']             = 'Вперёд';
+$lang['toolbar']['forward_hint']        = 'Перейти в следующую посещённую папку';
+$lang['toolbar']['up']                  = 'Вверх';
+$lang['toolbar']['up_hint']             = 'Перейти на уровень выше';
+$lang['toolbar']['root']                = 'Корень';
+$lang['toolbar']['root_hint']           = 'Перейти в корень файловой системы';
+$lang['toolbar']['home']                = 'Домой';
+$lang['toolbar']['home_hint']           = 'Перейти в домашнюю папку';
+$lang['toolbar']['refresh']             = 'Обновить';
+$lang['toolbar']['refresh_hint']        = 'Обновить список файлов и папок';
+$lang['toolbar']['new_file']            = 'Создать файл';
+$lang['toolbar']['new_file_hint']       = 'Создать пустой файл в текущей директории';
+$lang['toolbar']['new_dir']             = 'Создать папку';
+$lang['toolbar']['new_dir_hint']        = 'Создать папку в текущей директории';
+$lang['toolbar']['paste']               = 'Вставить';
+$lang['toolbar']['paste_hint']          = 'Вставить элемент из буфера обмена в текущую директорию';
+
+$lang['partbar']['refresh']             = 'Обновить';
+$lang['partbar']['refresh_hint']        = 'Обновить список разделов';
+$lang['partbar']['label']               = 'Список разделов:';
+$lang['partbar']['mount']               = 'Точка монтирования - ';
+
+$lang['addressbar']['label']            = 'Текущий каталог:';
+$lang['addressbar']['button']           = 'Сменить';
+$lang['addressbar']['button_hint']      = 'Перейти в указанную папку';
+
+$lang['column']['title']                = 'Название';
+$lang['column']['size']                 = 'Размер';
+$lang['column']['mtime']                = 'Дата изменения';
+
+$lang['statusbar']['count']             = 'Количество элементов:';
+$lang['statusbar']['dirs']              = 'папок:';
+$lang['statusbar']['files']             = 'файлов:';
+
+$lang['bookmarks']['title']             = 'Изменить закладки';
+$lang['bookmarks']['name']              = 'Название:';
+$lang['bookmarks']['path']              = 'Адрес:';
+$lang['bookmarks']['save']              = '_Сохранить изменения';
+$lang['bookmarks']['save_hint']         = 'Сохранить изменения, сделанные для данной закладки';
+$lang['bookmarks']['delete']            = '_Удалить';
+$lang['bookmarks']['delete_hint']       = 'Удалить выбранную закладку';
+$lang['bookmarks']['delete_all']        = 'Удалить _все';
+$lang['bookmarks']['delete_all_hint']   = 'Удалить все закладки';
+$lang['bookmarks']['add']               = '_Добавить';
+$lang['bookmarks']['add_hint']          = 'Добавить новую закладку с настройками по умолчанию   ';
+$lang['bookmarks']['bookmarks']         = 'Закладки';
+$lang['bookmarks']['root']              = 'Корень';
+$lang['bookmarks']['new']               = 'Новая закладка';
+
+$lang['about']['comments']              = 'Небольшой файловый менеджер, написанный на языке PHP'.
+' с использованием библиотеки PHP-GTK2.';
+$lang['about']['license']               = "Программа FlightFiles является свободным программным обеспечением.\n\n".
+"Вы вправе распространять ее и/или модифицировать\n".
+"в соответствии с условиями лицензии MIT.\n\n".
+"Разработчик не предоставляет каких-либо гарантий на программу.\n\n".
+"Вы используете её на свой страх и риск.\n\n".
+"Вместе с данной программой вы должны были получить экземпляр\n".
+"лицензии MIT, распологающийся в файле LICENSE.";
+
+$lang['shortcuts']['title']             = 'Сочетания клавиш';
+$lang['shortcuts']['up']                = 'Вверх';
+$lang['shortcuts']['back']              = 'Назад';
+$lang['shortcuts']['forward']           = 'Вперёд';
+$lang['shortcuts']['refresh']           = 'Обновить';
+$lang['shortcuts']['new_file']          = 'Создать файл';
+$lang['shortcuts']['new_dir']           = 'Создать папку';
+$lang['shortcuts']['close']             = 'Закрыть программу';
+$lang['shortcuts']['copy']              = 'Копировать';
+$lang['shortcuts']['cut']               = 'Вырезать';
+$lang['shortcuts']['paste']             = 'Вставить';
+$lang['shortcuts']['comand']            = 'Назначение';
+$lang['shortcuts']['shortcuts']         = 'Сочетание клавиш';
+$lang['shortcuts']['hidden_files']      = 'Показывать скрытые файлы';
+$lang['shortcuts']['rename']            = 'Переименовать';
+$lang['shortcuts']['toolbar']           = 'Панель инструментов';
+$lang['shortcuts']['addressbar']        = 'Адресная строка';
+$lang['shortcuts']['statusbar']         = 'Статусная панель';
+
+$lang['preference']['title']            = 'Параметры FlightFiles';
+$lang['preference']['hidden_files']     = 'Показывать скрытые файлы и папки';
+$lang['preference']['hidden_files_hint'] = 'Скрытыми считаются файлы, имя которых начинается с точки';
+$lang['preference']['ask_delete']       = 'Требовать подтверждение при удаления файлов/папок';
+$lang['preference']['ask_delete_hint']  = 'При попытке удаления файла/папки откроется диалоговое окно с требованием подтверждения операции';
+$lang['preference']['home_dir_left']    = 'Начальная директория для левой панели:';
+$lang['preference']['home_dir_right']   = 'Начальная директория для правой панели:';
+$lang['preference']['ask_close']        = 'Требовать подтверждение при выходе';
+$lang['preference']['ask_close_hint']   = 'При закрытии программы появится диалоговое окно, требующее подтверждения операции';
+$lang['preference']['general']          = 'Основные';
+$lang['preference']['font_list']        = 'Шрифт в списке:';
+$lang['preference']['change']           = '_Сменить';
+$lang['preference']['system_font']      = 'Использовать системный шрифт';
+$lang['preference']['fonts']            = 'Шрифты';
+$lang['preference']['lang']             = 'Язык программы:';
+$lang['preference']['lang_default']     = 'Язык по умолчанию';
+$lang['preference']['maximize']         = 'Разворачивать на весь экран при запуске';
+$lang['preference']['maximize_hint']    = 'При запуске главное окно FlightFiles будет развёрнуто на весь экран';
+$lang['preference']['program']          = 'Внешние программы';
+$lang['preference']['comparison']       = 'Сравнение файлов:';
+$lang['preference']['select_file']      = 'Выбрать файл';
+$lang['preference']['terminal']         = 'Терминал:';
+$lang['preference']['partbar_refresh']  = 'Автообновление списка разделов';
+$lang['preference']['partbar_refresh_hint'] = 'Список разделов будет автоматически обновляться каждую секунду';
+$lang['preference']['view_lines_files'] = 'Показывать линии между файлами';
+$lang['preference']['vlf_hint']         = 'Для лучшего визуального восприятия будут отображаться горизонтальные линии между файлами';
+$lang['preference']['view_lines_columns'] = 'Показывать линии между колонками';
+$lang['preference']['vlc_hint']         = 'Для лучшего визуального восприятия будут отображаться вертикальные линии между колонками';
+
+$lang['properties']['title']            = 'Свойства "%s"';
+$lang['properties']['name']             = 'Имя:';
+$lang['properties']['size']             = 'Размер:';
+$lang['properties']['path']             = 'Адрес:';
+$lang['properties']['mtime']            = 'Дата изменения:';
+$lang['properties']['atime']            = 'Дата доступа:';
+$lang['properties']['general']          = 'Основные';
+$lang['properties']['owner']            = 'Владелец:';
+$lang['properties']['group']            = 'Группа:';
+$lang['properties']['perms']            = 'Права доступа:';
+$lang['properties']['perms_text']       = 'В текстовом виде:';
+$lang['properties']['perms_tab']        = 'Права доступа';
+
+$lang['alert']['title']                 = 'Внимание';
+$lang['alert']['bufer_clear']           = 'Буфер обмена очищен';
+$lang['alert']['chmod_read_dir']        = 'У вас недостаточно прав для просмотра указанной директории!';
+$lang['alert']['chmod_read_file']       = 'У вас недостаточно прав для просмотра указанного файла!';
+$lang['alert']['empty_name']            = 'Необходимо ввести имя!';
+$lang['alert']['file_exists_rename']    = "Имя '%s' уже используется в текущей директории!";
+$lang['alert']['file_exists_paste']     = "Имя '%s' уже используется в текущей директории. Заменить?";
+$lang['alert']['dir_not_exists']        = 'Указанная вами директория не существует. Переход выполнен не будет.';
+$lang['alert']['new_not_chmod']         = 'У вас недостаточно прав на выполнение данной операции!';
+
+$lang['popup']['copy_file']             = 'Копировать';
+$lang['popup']['cut_file']              = 'Вырезать';
+$lang['popup']['rename_file']           = 'Переименовать';
+$lang['popup']['delete_file']           = 'Удалить';
+$lang['popup']['delete_active']         = 'Удалить выбранные';
+$lang['popup']['checksum']              = 'Контрольная сумма';
+$lang['popup']['md5']                   = 'MD5';
+$lang['popup']['sha1']                  = 'SHA1';
+$lang['popup']['open_text_file']        = 'Открыть в текстовом редакторе';
+$lang['popup']['open_dir']              = 'Открыть папку';
+$lang['popup']['copy_dir']              = 'Копировать';
+$lang['popup']['cut_dir']               = 'Вырезать';
+$lang['popup']['rename_dir']            = 'Переименовать';
+$lang['popup']['delete_dir']            = 'Удалить';
+$lang['popup']['new_file']              = 'Создать файл';
+$lang['popup']['new_dir']               = 'Создать папку';
+$lang['popup']['open_terminal']         = 'Открыть терминал здесь';
+$lang['popup']['paste']                 = 'Вставить';
+
+$lang['rename']['title']                = 'Переименовать';
+
+$lang['delete']['title']                = 'Удалить';
+$lang['delete']['dir']                  = 'Вы действительно хотите удалить папку "%s" со всем её содержимым?';
+$lang['delete']['file']                 = 'Вы действительно хотите удалить файл "%s"?';
+$lang['delete']['active']               = 'Вы действительно хотите удалить выбранные файлы/папки?';
+
+$lang['size']['b']                      = 'Б';
+$lang['size']['kib']                    = 'КиБ';
+$lang['size']['mib']                    = 'МиБ';
+$lang['size']['gib']                    = 'ГиБ';
+
+$lang['new']['file']                    = 'Новый файл';
+$lang['new']['dir']                     = 'Новая папка';
+
+$lang['font']['title']                  = 'Выбрать шрифт';
+$lang['font']['preview']                = 'Файловый менеджер FlightFiles';
+
+$lang['text_view']['title']             = 'Текстовый редактор';
+$lang['text_view']['statusbar']         = 'Файл:';
+
+$lang['text_editor_close']['title']      = 'Сохранить изменения';
+$lang['text_editor_close']['label']      = "Файл '%s' был изменён.\nСохранить изменения?";
+
+$lang['checksum']['title']              = 'Контрольная сумма - %s';
+$lang['checksum']['text']               = 'Файл - %s';
+
+$lang['close']['title']                 = 'Закрыть FlightFiles';
+$lang['close']['text']                  = 'Действительно закрыть FlightFiles?';
+
+$lang['command']['comparison_none']     = "Команда '%s' не найдена.";
+$lang['command']['comparison_empty']    = 'Не выбрана программа для сравнения файлов.';
+$lang['command']['terminal_none']       = "Команда '%s' не найдена.";
+$lang['command']['terminal_empty']      = 'Не выбрана программа в качестве терминала.';
+
+$lang['bulk_rename']['title']           = 'Массовое переименование';
+$lang['bulk_rename']['upper']           = '_Верхний регистр';
+$lang['bulk_rename']['upper_hint']      = 'Все буквы в именах файлов будут преобразованы в верхний регистр';
+$lang['bulk_rename']['lower']           = '_Нижний регистр';
+$lang['bulk_rename']['lower_hint']      = 'Все буквы в именах файлов будут преобразованы в нижний регистр';
+$lang['bulk_rename']['order']           = 'По пор_ядку';
+$lang['bulk_rename']['order_hint']      = "Имена файлов будут иметь на конце цифровой индекс, ".
+                                          "увеличивающийся на один для каждого следующего файла.\n".
+                                          "Пример: 'Файл 1', 'Файл 2', 'Файл 3' и т.д.";
+$lang['bulk_rename']['order_label']     = 'Имя для файлов:';
+$lang['bulk_rename']['order_default_name'] = 'Файл ';
+$lang['bulk_rename']['replace']         = '_Замена';
+$lang['bulk_rename']['replace_hint']    = 'В именах всех файлов строка поиска будет заменена на строку замены';
+$lang['bulk_rename']['replace_match']   = 'Строка поиска:';
+$lang['bulk_rename']['replace_replace'] = 'Строка замены:';
+$lang['bulk_rename']['ext']             = 'Оставить пр_ежние расширения';
+$lang['bulk_rename']['ext_hint']        = 'Расширения не будут подвергаться переименованию';
+$lang['bulk_rename']['hidden']          = 'Пропускать _скрытые файлы';
+$lang['bulk_rename']['hidden_hint']     = 'Скрытыми считаются файлы и папки, имена которых начинаются с точки';
+$lang['bulk_rename']['cancel']          = '_Отменить';
+$lang['bulk_rename']['rename']          = '_Переименовать';
